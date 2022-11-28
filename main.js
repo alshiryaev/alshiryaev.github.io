961a7e40667f8f2f795e9fbee3678a729633d451
Shiryaev Aleksey <shiryaevayu@psbank.ru>
2022-11-28 16:10:28 +0300

    main button

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const tg = globalThis.Telegram.WebApp;
 tg.MainButton.isVisible = true;
 tg.MainButton.text = "Auth";
+tg.MainButton.onClick = function() {
+    tg.showPopup({
+        title: 'Вот это да!',
+        message: 'Вы нажали MainButton'
+    })
+};
 console.log(tg);
